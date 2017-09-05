8b56bce925a45ba8ca0360e09288a1b86ce445db
ankur singh <as.ankursingh3.1@gmail.com>
2017-09-05 16:06:32 +0530

    stegnography completed

--- IS/steg.py
+++ IS/steg.py
@@ -1,27 +1,34 @@
 import numpy as np
-from PIL import Image
+import PIL 
 from scipy import misc
 
 def encry(text):
-	face = misc.face('face.png')
+	face = np.asarray(PIL.Image.open('face.png'))
 	shape = face.shape
+	for i in range(len(text)):
+		print (face[-1][i][0])
+	print ('\n')
+	face.setflags(write=1)
 	text = list(text)
 	for i in range(len(text)):
-		face[0][i][0] = ord(text[i])
-		print (face[0][i][0])
+		face[-1][i][0] = ord(text[i])
+		print (face[-1][i][0])
+	print ('\n')
 	#misc.imsave('face_en.png', face, format="png")
-	img = Image.fromarray(face, "RGB")
-	img.save('face_en.png')
-	face = misc.face('face_en.png')
+	#img = Image.fromarray(face, "RGB")
+	facen = PIL.Image.fromarray(np.uint8(face))
+	facen.save('facen.png')
+	#face = misc.face('facen.png')
+	'''
 	for i in range(len(text)):
-		print (face[0][i][0])
-	
+		print (facen[-1][i][0])
+	print ('\n')
+	'''
 
 
 def decry(length):
 	text = ''
-	face = misc.face('face_en.png')
-	shape = face.shape
+	facen = np.asarray(PIL.Image.open('facen.png'))
 	for i in range(length):
-		text = text + chr(face[0][i][0])
+		text = text + chr(facen[-1][i][0])
 	print (text)
